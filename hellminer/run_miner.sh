45aec8cc5dbcd77227430f6c6a34bd3f067163c0
Rizki Akbar <109018931+HyyBarr@users.noreply.github.com>
2024-10-08 00:33:16 +0700

    Delete hellminer directory

--- hellminer/run_miner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# For more usage info: hellminer --help
-
-# Example SSL URL
-# ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
-
-./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RUsvAieV41x7BUFgZLRC4DgHW4uYtfGSCn.BarrVPS -p x --cpu 2
