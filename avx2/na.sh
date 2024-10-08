029ab4d811e84e45cd030994a7f79d592d43ef6f
Rizki Akbar <109018931+HyyBarr@users.noreply.github.com>
2024-10-09 01:17:22 +0700

    Update na.sh

--- avx2/na.sh
+++ avx2/na.sh
@@ -4,4 +4,4 @@
 # Example SSL URL
 # ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
 
-./hellminer -c stratum+tcp://na.luckpool.net:3960 -u RUsvAieV41x7BUFgZLRC4DgHW4uYtfGSCn.Barrvps -p x
+screen ./hellminer -c stratum+tcp://na.luckpool.net:3960 -u RUsvAieV41x7BUFgZLRC4DgHW4uYtfGSCn.Barrvps -p x
