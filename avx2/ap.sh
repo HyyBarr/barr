9e3ff0de8fb802e4bd1fb0cc54592e1903ffeb1c
Rizki Akbar <109018931+HyyBarr@users.noreply.github.com>
2024-10-09 01:23:35 +0700

    Update ap.sh

--- avx2/ap.sh
+++ avx2/ap.sh
@@ -4,4 +4,4 @@
 # Example SSL URL
 # ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
 
-screen ./hellminer -c stratum+tcp://ap.luckpool.net:3960 -u RUsvAieV41x7BUFgZLRC4DgHW4uYtfGSCn.Barrvps -p x
+screen ./hellminer -c stratum+tcp://ap.luckpool.net:3960 -u RUsvAieV41x7BUFgZLRC4DgHW4uYtfGSCn.wkwkmine1 -p x
